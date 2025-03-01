bb56d5d5e5e6c16916252c724bd358de0d2896e5
Piers Taylor <piers.taylorr@gmail.com>
2025-03-01 11:32:35 +0000

    Day_30

---
+++ Day_30/main.py
@@ -0,0 +1,13 @@
+# try:
+#     file = open("Day_30/a_file.txt")
+#     dict = {"key": "value"}
+#     print(dict["key"])
+# except FileNotFoundError:
+#     open("Day_30/a_file.txt", "w")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     print(file.read())
+# finally:
+#     raise KeyError("There is an error I made up")
+
