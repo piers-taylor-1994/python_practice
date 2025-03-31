fb197a53a0d18ca74649f7ab5e37094fa9d72ceb
Piers Taylor <piers.taylorr@gmail.com>
2025-03-31 15:27:51 +0100

    leetcode practice

--- LeetCode/dynamic_programming/main.py
+++ LeetCode/dynamic_programming/main.py
@@ -152,6 +152,27 @@ class Solution:
         # Recursive calculation for paths
         memo[(m, n)] = self.unique_paths(m - 1, n, memo) + self.unique_paths(m, n - 1, memo)
         return memo[(m, n)]
+    
+    def coin_change_2(self, coins, amount):
+        def dp(amount, memo):
+            if amount == 0:
+                return 0
+            elif amount < 0:
+                return float('inf')
+            if amount not in memo:
+                memo[amount] = min([dp(amount - c, memo) for c in coins]) + 1
+            return memo[amount]
+        memo = {}
+        output = dp(amount, memo)
+        return output if output != float('inf') else -1
+    
+    def coin_change_2_2(self, coins, amount):
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] = min(dp[i], dp[i - c] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1
      
 solution = Solution()
 print(solution.fibo(8))
@@ -160,3 +181,9 @@ print(solution.coin_change([1,2], 4))
 print(solution.coin_change([1,2,5], 11))
 print(solution.coin_change([2], 3))
 print(solution.unique_paths(3, 3))
+print(solution.coin_change_2([1,2,5], 11))
+print(solution.coin_change_2([2], 3))
+print(solution.coin_change_2([1], 0))
+# print(solution.coin_change_2_2([1,2,5], 11))
+print(solution.coin_change_2_2([2], 3))
+print(solution.coin_change_2_2([1], 0))
