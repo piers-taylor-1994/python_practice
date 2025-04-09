58f0e5c1f5881c0e21bd01a00fc1a3fd6afa4843
Piers Taylor <piers.taylorr@gmail.com>
2025-04-09 16:37:36 +0100

    leetcode hash map/set practice + uniqueOccurences

--- LeetCode/hash_map_set.py
+++ LeetCode/hash_map_set.py
@@ -5,7 +5,24 @@ class Solution:
         :type nums2: List[int]
         :rtype: List[List[int]]
         """
-        pass
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return [list(set1 - set2), list(set2 - set1)]
+    
+    def uniqueOccurrences(self, arr:list):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        occurences_count = set()
+        for n in set(arr):
+            number_count = arr.count(n)
+            if number_count in occurences_count:
+                return False
+            occurences_count.add(number_count)
+        return True
 
 solution = Solution()
 print(solution.findDifference([1,2,3], [2,4,6]))
+print(solution.uniqueOccurrences([1,2,2,1,1,3]))
+print(solution.uniqueOccurrences([1,2]))
