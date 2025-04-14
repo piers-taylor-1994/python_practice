52388135f6849d33987e8a08061d6a06f75bd096
Piers Taylor <piers.taylorr@gmail.com>
2025-04-14 17:08:43 +0100

    leetcode udemy container_with_most_water

--- LeetCode/udemy_course/arrays.py
+++ LeetCode/udemy_course/arrays.py
@@ -1,5 +1,5 @@
 class Solution:
-    def two_sum_brute_force(self, nums, target):
+    def two_sum_bf(self, nums, target):
         for left in range(0, len(nums) - 1):
             number_to_find = target - nums[left]
             for right in range(left + 1, len(nums)):
@@ -14,8 +14,42 @@ class Solution:
                 return [number_index, i]
             number_to_find = target - nums[i]
             numbers_to_find_dict[number_to_find] = i
+
+    def container_with_most_water_bf(self, heights:list[int]):
+        max_area = 0
+        for p1 in range(len(heights) - 1):
+            for p2 in range(p1 + 1, len(heights)):
+                min_height = min(heights[p1], heights[p2])
+                width = p2 - p1
+                current_area = min_height * width
+                max_area = max(max_area, current_area)
+        return max_area
+    def container_with_most_water(self, heights:list[int]):
+        max_area = 0
+        p1 = 0
+        p2 = len(heights) - 1
+
+        while p1 < p2:
+            height = min(heights[p1], heights[p2])
+            width = p2 - p1
+            current_area = height * width
+            if current_area > max_area:
+                max_area = current_area
+
+            if max(heights[p1:p2]) * (p2 - p1 - 1) < max_area:
+                break
+
+            if heights[p1] < heights[p2]:
+                p1 += 1
+            else:
+                p2 -= 1
+        return max_area
+    
 solution = Solution()
-print(solution.two_sum_brute_force([1,3,7,9,2], 11))
-print(solution.two_sum_brute_force([3,2,4], 6))
+print(solution.two_sum_bf([1,3,7,9,2], 11))
+print(solution.two_sum_bf([3,2,4], 6))
 print(solution.two_sum([1,3,7,9,2], 11))
 print(solution.two_sum([3,2,4], 6))
+
+print(solution.container_with_most_water_bf([7,1,2,3,9]))
+print(solution.container_with_most_water([7,1,2,3,9]))
