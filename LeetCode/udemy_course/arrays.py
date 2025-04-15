fe63ad64be1ec2fe094bd8f7d21959ce3797f4c7
Piers Taylor <piers.taylorr@gmail.com>
2025-04-15 17:39:37 +0100

    leetcode udemy trapped_rainwater

--- LeetCode/udemy_course/arrays.py
+++ LeetCode/udemy_course/arrays.py
@@ -41,6 +41,48 @@ class Solution:
                 p2 -= 1
         return max_area
     
+    def trapping_rainwater_bf(self, heights:list[int]):
+        water_trapped = 0
+        
+        for p1 in range(1, len(heights) - 1):
+            max_left = 0
+            max_right = 0
+            left = p1
+            right = p1
+            while left >= 0:
+                max_left = max(max_left, heights[left])
+                left -= 1
+            while right < len(heights):
+                max_right = max(max_right, heights[right])
+                right += 1
+
+            current_water = min(max_left, max_right) - heights[p1]
+            if current_water > 0:
+                water_trapped += current_water
+        return water_trapped
+        
+    def trapping_rainwater(self, heights:list[int]):
+        water_trapped = 0
+        max_left = 0
+        max_right = 0
+        left = 0
+        right = len(heights) - 1
+        
+        while left < right:
+            if heights[left] <= heights[right]:
+                if heights[left] >= max_left:
+                    max_left = heights[left]
+                else:
+                    water_trapped += max_left - heights[left]
+                left += 1
+            else:
+                if heights[right] >= max_right:
+                    max_right = heights[right]
+                else:
+                    water_trapped += max_right - heights[right]
+                right -= 1
+        return water_trapped
+    
 solution = Solution()
 print(solution.two_sum_bf([1,3,7,9,2], 11))
 print(solution.two_sum_bf([3,2,4], 6))
@@ -49,3 +91,8 @@ print(solution.two_sum([3,2,4], 6))
 
 print(solution.container_with_most_water_bf([7,1,2,3,9]))
 print(solution.container_with_most_water([7,1,2,3,9]))
+
+print(solution.trapping_rainwater_bf([0,1,0,2,1,0,3,1,0,1,2]))
+print(solution.trapping_rainwater_bf([0,1,0,2,1,0,1,3,2,1,2,1]))
+print(solution.trapping_rainwater([0,1,0,2,1,0,3,1,0,1,2]))
+print(solution.trapping_rainwater([0,1,0,2,1,0,1,3,2,1,2,1]))
