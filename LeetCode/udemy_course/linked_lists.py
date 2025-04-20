ca409f7e1aa48a6fb39c73079495a28675e426e3
Piers Taylor <piers.taylorr@gmail.com>
2025-04-20 10:56:29 +0100

    leetcode udemy linked_lists added notes

--- LeetCode/udemy_course/linked_lists.py
+++ LeetCode/udemy_course/linked_lists.py
@@ -5,7 +5,7 @@ class Solution:
         
         while current:
             next = current.next #Record the next node before we change the data around
-            current.next = prev #Reverse the pointers (as we're reversing the list)
+            current.next = prev #Reverse the pointers (as we're reversing the list) (before this, it's still pointing forward 1=>2=>3, we want it the reverse 3=>2=>1=>null etc)
             prev = current #Record the current state of the linked array (move the previous prev forward)
             current = next #Move to the next node
         return prev
