9e4ab1ea36710570e73a27b31c5c3adb6fc47541
Piers Taylor <piers.taylorr@gmail.com>
2025-04-21 19:27:22 +0100

    leetcode udemy partial_reversed_lists practiced with comments

--- LeetCode/udemy_course/linked_lists.py
+++ LeetCode/udemy_course/linked_lists.py
@@ -48,31 +48,37 @@ class Solution:
         return prev
     
     def reverse_partial_linked_list(self, head, left, right):
-        position = 1
-        current = head
+        current_position = 1
+        current_node = head
         start = head
 
-        while position < left:
-            start = current
-            current = current.next
-            position += 1
+        #While current_position is before the soon-to-be reversed part
+        while current_position < left:
+            start = current_node #start will always eventually be the node before the reversed part (left - 1)
+            current_node = current_node.next
+            current_position += 1
         
-        tail = current
-        new_list = None
+        #Since the previous while loop has finished, that means we're in the reversed part therefore tail is the current node
+        tail = current_node
+        new_reversed_list = None #Used to be called prev
 
-        while position >= left and position <= right:
-            next = current.next
-            current.next = new_list
-            new_list = current
-            current = next
-            position += 1
+        #Same as in the previous solution, apart from iterating current_position
+        while current_position >= left and current_position <= right:
+            next = current_node.next
+            current_node.next = new_reversed_list
+            new_reversed_list = current_node
+            current_node = next
+            current_position += 1
 
-        start.next = new_list
-        tail.next = current
+        #While drawing out the before and after, highlighting the node before (start) and the tail of the reversed list part (tail) makes it obvious which points at what
+        start.next = new_reversed_list #The start (before reversed part) equals to the new reversed list
+        tail.next = current_node #The end of the reversed list now points at the node after we need to reverse
 
+        #This is to make sure that they don't want the reversing to start at the beginning. If they do, head won't be valid anymore, as it'll be now the tail.
+        #Therefore we want to return reversed_list, as this is the head of the new reversed_list (the last value of new list is the new head thefore == new_versed_list)
         if left > 1:
             return head
-        return new_list
+        return new_reversed_list
     
 solution = Solution()
 linked_list = LinkedList()
