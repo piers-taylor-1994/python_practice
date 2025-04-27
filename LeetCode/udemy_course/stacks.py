ffeb18bbe74ca3b67077e6f7df70b9073be4fa6d
Piers Taylor <piers.taylorr@gmail.com>
2025-04-27 19:22:14 +0100

    leetcode udemy stacks minimum_brackets

--- LeetCode/udemy_course/stacks.py
+++ LeetCode/udemy_course/stacks.py
@@ -14,7 +14,35 @@ class Solution:
                         return False
                 else:
                     return False
-        return len(stack) == 0     
+        return len(stack) == 0
+
+    def minimum_brackets_to_remove(self, s):
+        stack = []
+        s_list = list(s)
+
+        for pos in range(len(s_list)):
+            if s_list[pos] in "()":
+                if s_list[pos] == "(":
+                    stack.append(pos)
+                else:
+                    if stack:
+                        stack.pop()
+                    else:
+                        s_list[pos] = ""
+        
+        if stack:
+            for pos in stack:
+                s_list[pos] = ""
+        return "".join(s_list)
+
+
 
 solution = Solution()
 print(solution.valid_parentheses("()"))
+print(solution.minimum_brackets_to_remove("a)cbc(d)"))
+print(solution.minimum_brackets_to_remove("(ab(c)d"))
+print(solution.minimum_brackets_to_remove("))(("))
+print(solution.minimum_brackets_to_remove("())()((("))
+
+test = {0: "Hello", 1: "Test"}
+print(test.popitem())
