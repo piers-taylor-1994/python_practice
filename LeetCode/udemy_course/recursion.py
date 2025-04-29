c449f3856a4285848c890c34e2d099ceed6e2ced
Piers Taylor <piers.taylorr@gmail.com>
2025-04-29 16:16:28 +0100

    leetcode udemy recursion factorial with tail_recursion

--- LeetCode/udemy_course/recursion.py
+++ LeetCode/udemy_course/recursion.py
@@ -4,6 +4,13 @@ class Solution:
             return 1
         else:
             return x * self.factorial(x - 1)
+    
+    def factorial_tail(self, x, total_so_far = 1):
+        if x == 1:
+            return total_so_far
+        else:
+            return self.factorial_tail(x - 1, total_so_far * x)
 
 solution = Solution()
 print(solution.factorial(4))
+print(solution.factorial_tail(4))
