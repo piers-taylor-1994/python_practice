b6bcfc9eb453946bed167e5136cddb6a1448e22a
Piers Taylor <piers.taylorr@gmail.com>
2025-05-15 18:36:01 +0100

    leetcode udemy heaps priority_queue setup

---
+++ LeetCode/heaps_priority_queues.py
@@ -0,0 +1,35 @@
+class PriorityQueue:
+    def __init__(self, max_heap = True):
+        self.heap = [50,25,45,35,10,15,20]
+        self.max_heap = max_heap
+        pass
+
+    def comparator(self, a, b):
+        if self.max_heap:
+            return a > b
+        else:
+            return a < b
+        
+    def size(self):
+        return len(self.heap)
+    
+    def is_empty(self):
+        return len(self.heap) == 0
+    
+    def parent(self, idx):
+        return (idx - 1) // 2
+    
+    def left_child(self, idx):
+        return (idx * 2) + 1
+    
+    def right_child(self, idx):
+        return (idx * 2) + 2
+    
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+    
+    def peek(self):
+        return self.heap[0]
+
+priority_queue = PriorityQueue()
+print(priority_queue.heap)
