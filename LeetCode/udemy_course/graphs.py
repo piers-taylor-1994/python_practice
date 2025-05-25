29ec0220ce5584e02434db64f389b2ff28ab6653
Piers Taylor <piers.taylorr@gmail.com>
2025-05-25 19:28:03 +0100

    leetcode udemy graphs dfs

--- LeetCode/udemy_course/graphs.py
+++ LeetCode/udemy_course/graphs.py
@@ -17,9 +17,36 @@ class Solution:
                     seen[connection] = True
         
         return result
+    
+    def traversal_dfs(self, graph):
+        def dfs(node, seen, result):
+            if node in seen:
+                return
+            
+            result.append(node)
+            seen.add(node)
+            
+            for edge in graph[node]:
+                dfs(edge, seen, result)
+
+        result = []
+        seen = set()
+        dfs(0, seen, result)
+        return result
 
 solution = Solution()
 print(solution.traversal_bfs([
+    [1, 3],
+    [0],
+    [3, 8],
+    [0, 4, 5, 2],
+    [3, 6],
+    [3],
+    [4, 7],
+    [6],
+    [2]
+]))
+print(solution.traversal_dfs([
     [1, 3],
     [0],
     [3, 8],
