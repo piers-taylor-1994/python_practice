54f699fe03110e79d6088b4530b3c5d0c745b878
Piers Taylor <piers.taylorr@gmail.com>
2025-05-26 17:22:03 +0100

    leetcode udemy graphs bfs

--- LeetCode/udemy_course/graphs.py
+++ LeetCode/udemy_course/graphs.py
@@ -2,37 +2,27 @@ from collections import deque
 
 class Solution:
     def traversal_bfs(self, graph):
+        if not graph:
+            return []
+        
         result = []
-        seen = {}
+        seen = set()
         queue = deque([0])
-        seen[0] = True
+        seen.add(0)
 
         while queue:
-            vertex = queue.popleft()
-            result.append(vertex)
+            node = queue.popleft()
+            result.append(node)
 
-            for connection in graph[vertex]:
-                if not connection in seen:
-                    queue.append(connection)
-                    seen[connection] = True
+            for edge in graph[node]:
+                if edge not in seen:
+                    queue.append(edge)
+                    seen.add(edge)
         
         return result
     
     def traversal_dfs(self, graph):
-        def dfs(node, seen, result):
-            if node in seen:
-                return
-            
-            result.append(node)
-            seen.add(node)
-            
-            for edge in graph[node]:
-                dfs(edge, seen, result)
-
-        result = []
-        seen = set()
-        dfs(0, seen, result)
-        return result
+        pass
 
 solution = Solution()
 print(solution.traversal_bfs([
