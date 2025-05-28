cffb657d29f34481d50a36a78516dc5ea021b919
Piers Taylor <piers.taylorr@gmail.com>
2025-05-28 16:16:03 +0100

    leetcode udemy graphs bfs

--- LeetCode/udemy_course/graphs.py
+++ LeetCode/udemy_course/graphs.py
@@ -6,8 +6,8 @@ class Solution:
             return []
         
         result = []
-        seen = set()
         queue = deque([0])
+        seen = set()
         seen.add(0)
 
         while queue:
@@ -18,49 +18,22 @@ class Solution:
                 if edge not in seen:
                     queue.append(edge)
                     seen.add(edge)
-        
         return result
     
     def traversal_dfs(self, graph):
-        if not graph:
-            return []
-        
-        def dfs(node, seen, result):
-            if node in seen:
-                return
-            
-            result.append(node)
-            seen.add(node)
-
-            for edge in graph[node]:
-                dfs(edge, seen, result)
-        
-        result = []
-        seen = set()
-        dfs(0, seen, result)
-
-        return result
+        pass
 
 solution = Solution()
-print(solution.traversal_bfs([
-    [1, 3],
-    [0],
-    [3, 8],
-    [0, 4, 5, 2],
-    [3, 6],
-    [3],
-    [4, 7],
-    [6],
-    [2]
-]))
-print(solution.traversal_dfs([
-    [1, 3],
-    [0],
-    [3, 8],
-    [0, 4, 5, 2],
-    [3, 6],
-    [3],
-    [4, 7],
-    [6],
-    [2]
-]))
+graph_1 = [
+    [1, 3], #0
+    [0], #1
+    [3, 8], #2
+    [0, 4, 5, 2], #3
+    [3, 6], #4
+    [3], #5
+    [4, 7], #6
+    [6], #7
+    [2] #8
+]
+print(solution.traversal_bfs(graph_1))
+print(solution.traversal_dfs(graph_1))
