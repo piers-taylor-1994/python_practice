6262fab0d1a44434fc4fa6fcf78f11f3b820c683
Piers Taylor <piers.taylorr@gmail.com>
2025-06-08 19:40:19 +0100

    leetcode udemy dp cost_stairs tabular

--- LeetCode/udemy_course/dynamic_programming.py
+++ LeetCode/udemy_course/dynamic_programming.py
@@ -13,7 +13,18 @@ class Solution:
 
         n = len(cost)
         return 0 + min(dp(n-1, {}), dp(n-2, {}))
+    
+    def min_cost_stairs_tabular(self, cost):
+        dp = {}
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        
+        return min(dp[len(cost) - 1], dp[len(cost) - 2])
 
 
 solution = Solution()
 print(solution.min_cost_stairs_memo([20,15,30,5]))
+print(solution.min_cost_stairs_tabular([20,15,30,5]))
