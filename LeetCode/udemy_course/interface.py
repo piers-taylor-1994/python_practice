2562c03d3c2675ba32c3320eb055cf10f8ae595a
Piers Taylor <piers.taylorr@gmail.com>
2025-07-02 11:48:58 +0100

    leetcode udemy interface monarchy practice

--- LeetCode/udemy_course/interface.py
+++ LeetCode/udemy_course/interface.py
@@ -1,11 +1,10 @@
 from collections import deque
 
-
-class Node():
+class Node:
     def __init__(self, name):
         self.Name = name
-        self.Children = []
         self.Alive = True
+        self.Children = []
         
 class ThroneInheritance(object):
     def __init__(self, kingName):
@@ -13,7 +12,6 @@ class ThroneInheritance(object):
         :type kingName: str
         """
         self.head = Node(kingName)
-        self.lookup = {kingName: self.head}
 
     def birth(self, parentName, childName):
         """
@@ -21,17 +19,30 @@ class ThroneInheritance(object):
         :type childName: str
         :rtype: None
         """
-        parent = self.lookup[parentName]
-        child = Node(childName)
-        parent.Children.append(child)
-        self.lookup[childName] = child
+        current = self.head
+        queue = deque([current])
 
+        while queue:
+            current = queue.popleft()
+            if current.Name == parentName:
+                current.Children.append(Node(childName))
+                break
+            queue += current.Children
+        
     def death(self, name):
         """
         :type name: str
         :rtype: None
         """
-        self.lookup[name].Alive = False
+        current = self.head
+        queue = deque([current])
+
+        while queue:
+            current = queue.popleft()
+            if current.Name == name:
+                current.Alive = False
+                break
+            queue += current.Children
 
     def getInheritanceOrder(self):
         """
@@ -39,14 +50,15 @@ class ThroneInheritance(object):
         """
         results = []
 
-        def dfs(node):
+        def rec(node):
             if node.Alive:
                 results.append(node.Name)
             for child in node.Children:
-                dfs(child)
+                rec(child)
 
-        dfs(self.head)
-        print(results)
+        rec(self.head)
+        return results
+
 
 
 # Your ThroneInheritance object will be instantiated and called as such:
@@ -65,7 +77,7 @@ t = ThroneInheritance("king")
 t.birth("king", "clyde")
 t.death("king")
 t.birth("clyde","shannon")
-t.getInheritanceOrder()
+print(t.getInheritanceOrder())
 t.birth("shannon","scott")
 t.death("clyde")
-t.getInheritanceOrder()
+print(t.getInheritanceOrder())
