7588dc5217d73f941ce3a989444144641a524447
Piers Taylor <piers.taylorr@gmail.com>
2025-07-06 12:04:49 +0100

    heap practice

--- LeetCode/udemy_course/heaps.py
+++ LeetCode/udemy_course/heaps.py
@@ -3,18 +3,61 @@ import heapq
 class MinHeap:
     def __init__(self):
         self.heap = []
-        ...
+
+    def __len__(self):
+        return len(self.heap)
+
+    def parent(self, i):
+        return (i - 1) // 2
+    
+    def left_child(self, i):
+        return (i * 2) + 1
+    
+    def right_child(self, i):
+        return (i * 2) + 2
     
     def insert(self, val):
         # Append value and bubble it up
-        ...
+        self.heap.append(val)
+
+        val_idx = len(self.heap) - 1
+
+        while val_idx > 0:
+            parent_idx = self.parent(val_idx)
+
+            if self.heap[val_idx] < self.heap[parent_idx]:
+                self.heap[val_idx], self.heap[parent_idx] = self.heap[parent_idx], self.heap[val_idx]
+                val_idx = parent_idx
+            else:
+                break
     
     def extract_min(self):
         # Swap root with last, remove last, bubble root down
-        ...
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+
+        min_val = self.heap.pop()
+
+        root_idx = 0
+
+        while root_idx < len(self.heap):
+            smallest = root_idx
+            left_child_idx = self.left_child(root_idx)
+            right_child_idx = self.right_child(root_idx)
+
+            if left_child_idx < len(self.heap) and self.heap[left_child_idx] < self.heap[smallest]:
+                smallest = left_child_idx
+            if right_child_idx < len(self.heap) and self.heap[right_child_idx] < self.heap[smallest]:
+                smallest = right_child_idx
+            if smallest == root_idx:
+                break
+
+            self.heap[root_idx], self.heap[smallest] = self.heap[smallest], self.heap[root_idx]
+            root_idx = smallest
+        
+        return min_val
 
     def peek(self):
-        ...
+        return self.heap[0] if self.heap else None
 
     
 heap = MinHeap()
