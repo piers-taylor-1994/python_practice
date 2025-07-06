f0b6ff89f70858c4ffac1c684d4577454dac2c4d
Piers Taylor <piers.taylorr@gmail.com>
2025-07-06 15:57:02 +0100

    heap practice

--- LeetCode/udemy_course/heaps.py
+++ LeetCode/udemy_course/heaps.py
@@ -8,7 +8,7 @@ class MinHeap:
         return len(self.heap)
 
     def parent(self, i):
-        return (i - 1) // 2
+        return (i-1) // 2
     
     def left_child(self, i):
         return (i * 2) + 1
@@ -16,49 +16,72 @@ class MinHeap:
     def right_child(self, i):
         return (i * 2) + 2
     
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+    
     def insert(self, val):
         # Append value and bubble it up
         self.heap.append(val)
 
-        val_idx = len(self.heap) - 1
+        val_idx = self.__len__() - 1
 
         while val_idx > 0:
             parent_idx = self.parent(val_idx)
 
             if self.heap[val_idx] < self.heap[parent_idx]:
-                self.heap[val_idx], self.heap[parent_idx] = self.heap[parent_idx], self.heap[val_idx]
+                self.swap(val_idx, parent_idx)
                 val_idx = parent_idx
             else:
                 break
     
     def extract_min(self):
         # Swap root with last, remove last, bubble root down
-        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        self.swap(0, self.__len__() - 1)
 
-        min_val = self.heap.pop()
+        min_value = self.heap.pop()
 
         root_idx = 0
 
-        while root_idx < len(self.heap):
-            smallest = root_idx
+        while root_idx < self.__len__():
+            smallest_idx = root_idx
             left_child_idx = self.left_child(root_idx)
             right_child_idx = self.right_child(root_idx)
 
-            if left_child_idx < len(self.heap) and self.heap[left_child_idx] < self.heap[smallest]:
-                smallest = left_child_idx
-            if right_child_idx < len(self.heap) and self.heap[right_child_idx] < self.heap[smallest]:
-                smallest = right_child_idx
-            if smallest == root_idx:
+            if left_child_idx < self.__len__() and self.heap[left_child_idx] < self.heap[smallest_idx]:
+                smallest_idx = left_child_idx
+            if right_child_idx < self.__len__() and self.heap[right_child_idx] < self.heap[smallest_idx]:
+                smallest_idx = right_child_idx
+            if smallest_idx == root_idx:
                 break
 
-            self.heap[root_idx], self.heap[smallest] = self.heap[smallest], self.heap[root_idx]
-            root_idx = smallest
+            self.swap(root_idx, smallest_idx)
+            root_idx = smallest_idx
         
-        return min_val
+        return min_value
 
     def peek(self):
         return self.heap[0] if self.heap else None
+    
+    def heapify(self, arr):
+        self.heap = arr
 
+        for i in range(len(arr) - 1, -1, -1):
+            idx = i
+
+            while idx < self.__len__():
+                smallest_idx = idx
+                left_child_idx = self.left_child(idx)
+                right_child_idx = self.right_child(idx)
+
+                if left_child_idx < self.__len__() and self.heap[left_child_idx] < self.heap[smallest_idx]:
+                    smallest_idx = left_child_idx
+                if right_child_idx < self.__len__() and self.heap[right_child_idx] < self.heap[smallest_idx]:
+                    smallest_idx = right_child_idx
+                if smallest_idx == idx:
+                    break
+
+                self.swap(idx, smallest_idx)
+                idx = smallest_idx
     
 heap = MinHeap()
 heap.insert(5)
@@ -66,8 +89,10 @@ heap.insert(3)
 heap.insert(8)
 heap.insert(1)
 heap.insert(2)
-print(heap.heap)
+# print(heap.heap)
 heap.extract_min()
+# print(heap.heap)
+heap.heapify([5,3,8,1,2])
 print(heap.heap)
 
 heap_2 = []
@@ -76,6 +101,9 @@ heapq.heappush(heap_2, 3)
 heapq.heappush(heap_2, 8)
 heapq.heappush(heap_2, 1)
 heapq.heappush(heap_2, 2)
-print(heap_2)
+# print(heap_2)
 heapq.heappop(heap_2)
+# print(heap_2)
+heap_2 = [5,3,8,1,2]
+heapq.heapify(heap_2)
 print(heap_2)
