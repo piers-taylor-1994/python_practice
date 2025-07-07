fb7ec9045cd6abba7e2d765d6b81d3c0353db1e9
Piers Taylor <piers.taylorr@gmail.com>
2025-07-07 12:58:47 +0100

    binary_search practice

--- LeetCode/udemy_course/sorts_searches.py
+++ LeetCode/udemy_course/sorts_searches.py
@@ -12,6 +12,23 @@ class Solution:
         right = [i for i in nums if i > pivot]
 
         return self.quick_sort(left) + middle + self.quick_sort(right)
+    
+    def binary_search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
+            else:
+                left = middle + 1
+        
+        return -1
 
 solution = Solution()
 print(solution.quick_sort([5, 2, 1, 9, 0, 3]))
+print(solution.binary_search([0,1,5,6,9], 6))
