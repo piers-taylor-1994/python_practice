e1d9e69c71cdcfb1c509d790212a2ca3058ab35e
Piers Taylor <piers.taylorr@gmail.com>
2025-07-07 13:53:10 +0100

    linked_list_has_cycle practice

--- LeetCode/udemy_course/practice.py
+++ LeetCode/udemy_course/practice.py
@@ -1,10 +1,16 @@
-from collections import deque
 import random
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     """
     1. Arrays
     2. Strings
+    3. Sorts/searches
+    4. Linked lists
     """
     def two_sum(self, nums, target):
         recorded_nums = {}
@@ -17,25 +23,48 @@ class Solution:
         return rec(0)
     
     def is_palindrome(self, s):
-        word = [letter for letter in s if letter in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"]
+        word = "".join([letter for letter in s if letter in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"]).lower()
         
-        queue = deque(word)
-        
-        while queue:
-            if len(queue) < 2:
-                break
-            letter_1 = queue.popleft()
-            letter_2 = queue.pop()
+        left = 0
+        right = len(word) - 1
 
-            if letter_1.lower() != letter_2.lower():
+        while left < right:
+            if word[left] != word[right]:
                 return False
-        
+            left += 1
+            right -= 1
         return True
+    
+    def linked_list_has_cycle(self, head):
+        if not head or not head.next:
+            return False
+        
+        nodes = []
+        current_node = head
+
+        while current_node:
+            if current_node in nodes:
+                return True
+            nodes.append(current_node)
+            current_node = current_node.next
 
 # print(random.choice(["container-with-most-water", "trapping-rainwater"]))
-print(random.choice(["typed-out-strings", "longest-substring-without-repeating", "is_almost_palindrome"]))
+# print(random.choice(["typed-out-strings", "longest-substring-without-repeating", "is_almost_palindrome"]))
+# print(random.choice(["quick_sort", "binary_search"]))
+print(random.choice(["reverse_partial_linked_list", "flatten_double_linked_list"]))
 
 solution = Solution()
+
 print(solution.two_sum([3,2,4], 6))
 
 print(solution.is_palindrome("A man, a plan, a canal: Panama"))
+
+head = ListNode(3)
+node_1 = ListNode(2)
+node_2 = ListNode(0)
+node_3 = ListNode(-4)
+head.next = node_1
+node_1.next = node_2
+node_2.next = node_3
+node_3.next = node_1
+print(solution.linked_list_has_cycle(head))
