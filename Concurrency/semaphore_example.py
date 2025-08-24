59b76721758639df66cce9adc151ea3594ce85ea
Piers Taylor <piers.taylorr@gmail.com>
2025-08-24 22:02:21 +0100

    concurrency practice

--- Concurrency/semaphore_example.py
+++ Concurrency/semaphore_example.py
@@ -1,23 +1,23 @@
-import random
 import threading
 import time
 
-connection_pool = threading.Semaphore(3)
+semaphore = threading.Semaphore(3)
 lock = threading.Lock()
 counter = 0
 
-def counter_increment(task_id):
+def increment_counter(thread_id):
     global counter
-    print(f"Task {task_id} waiting for connection\n")
-    
-    with connection_pool:
-        print(f"Task {task_id} ready to work\n")
-        time.sleep(random.uniform(0.5, 1.5))
+    print(f"Thread {thread_id} awaiting connection")
+
+    with semaphore:
+        print(f"Thread {thread_id} ready to work")
+        time.sleep(0.1)
+        
         with lock:
-            print(f"Task {task_id} doing critical work\n")
+            print(f"Thread {thread_id} doing critical work")
             counter += 1
 
-threads = [threading.Thread(target=counter_increment, args=(i,)) for i in range(9)]
+threads = [threading.Thread(target=increment_counter, args=(i, )) for i in range(9)]
 
 for t in threads:
     t.start()
@@ -25,4 +25,4 @@ for t in threads:
 for t in threads:
     t.join()
 
-print(f"All tasks complete, counter: {counter}")
+print(f"Threads all finished. Counter: {counter}")
