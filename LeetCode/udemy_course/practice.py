1f8f016114db75ecc0dc88c4130e2be3fe7c3169
piers taylor <piers.taylor@medway.gov.uk>
2025-09-03 12:54:26 +0100

    pointers practice

--- LeetCode/udemy_course/practice.py
+++ LeetCode/udemy_course/practice.py
@@ -82,25 +82,47 @@ class Trie:
         return True
     
 class Solution:
-    def jump(self, nums):
-        current_jump = 0
-        max_jump = 0
-        jumps = 0
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        results = []
 
-        for i in range(len(nums) - 1):
-            if i + nums[i] > max_jump:
-                max_jump = i + nums[i]
-            
-            if i == current_jump:
-                jumps += 1
-                current_jump = max_jump
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    
+                    while right > left and nums[right] == nums[right + 1]:
+                        right -= 1
         
-        return jumps
+        return results
 
 
 solution = Solution()
 
-print(solution.jump([2,1,1,1,1]))
+print(solution.threeSum([-1,0,1,2,-1,-4]))
+print(solution.threeSum([0, 1, 1]))
+print(solution.threeSum([0, 0, 0]))
 
 # print(random.choice([]))
 # print(random.choice(["typed-out-strings"]))
