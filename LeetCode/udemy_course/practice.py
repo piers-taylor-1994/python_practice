a342fe3353c746d40425153b82c3f6a9c8151e97
Piers Taylor <piers.taylorr@gmail.com>
2025-10-04 17:13:38 +0100

    heap practice

--- LeetCode/udemy_course/practice.py
+++ LeetCode/udemy_course/practice.py
@@ -83,51 +83,23 @@ class Trie:
         return True
     
 class Solution:
-    def mergeKLists(self, lists):
+    def topKFrequent(self, nums, k):
         """
-        :type lists: List[Optional[ListNode]]
-        :rtype: Optional[ListNode]
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
         """
-        if not lists or all(node == None for node in lists):
-            return None
+        num_count = Counter(nums)
 
-        min_heap = []
-        heapq.heapify(min_heap)
-
-        i = 0
-
-        for node in lists:
-            while node:
-                heapq.heappush(min_heap, (node.val, i, node))
-                i += 1
-                node = node.next
-
-        _, _, current_node = heapq.heappop(min_heap)
-        head = current_node
-        while current_node and min_heap:
-            _, _, next_node = heapq.heappop(min_heap)
-            current_node.next = next_node
-            current_node = current_node.next
-
-        return head
+        max_heap = []
+        for num, count in num_count.items():
+            heapq.heappush(max_heap, (-count, num))
+        
+        return [heapq.heappop(max_heap)[1] for _ in range(k)]
                 
 solution = Solution()
-node_1 = Node(1)
-node_4 = Node(4)
-node_5 = Node(5)
-node_1.next = node_4
-node_4.next = node_5
 
-node_1_2 = Node(1)
-node_3 = Node(3)
-node_4_2 = Node(4)
-node_1_2.next = node_3
-node_3.next = node_4_2
-
-node_2 = Node(2)
-node_6 = Node(6)
-node_2.next = node_6
-print(solution.mergeKLists([node_1, node_1_2, node_2]))
+print(solution.topKFrequent([1,1,1,2,2,3], 2))
 
 # print(random.choice([]))
 # print(random.choice(["typed-out-strings"]))
