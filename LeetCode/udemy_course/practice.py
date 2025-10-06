96c2870274aafa9cea9c901a6af0376b949772a5
Piers Taylor <piers.taylorr@gmail.com>
2025-10-06 17:09:00 +0100

    2 pointers practice

--- LeetCode/udemy_course/practice.py
+++ LeetCode/udemy_course/practice.py
@@ -83,24 +83,45 @@ class Trie:
         return True
     
 class Solution:
-    def jumpGame(self, nums):
-        current_jump = 0
-        longest_jump = 0
-        steps = 0
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        results = []
 
-        for i in range(len(nums) - 1):
-            longest_jump = max(longest_jump, i + nums[i])
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-            if i == current_jump:
-                steps += 1
-                current_jump = longest_jump
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0 :
+                    right -= 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while right > left and nums[right] == nums[right + 1]:
+                        right -= 1
         
-        return steps
+        return results
         
 
 solution = Solution()
 
-print(solution.jumpGame([2,3,1,1,4]))
+print(solution.threeSum([-1,0,1,2,-1,-4]))
 
 # print(random.choice([]))
 # print(random.choice(["typed-out-strings"]))
