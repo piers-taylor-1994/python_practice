2b762583461f563fda5b7a61db1c0f7946b9918a
Piers Taylor <piers.taylorr@gmail.com>
2025-10-21 13:39:38 +0100

    javascript practice

---
+++ LeetCode/udemy_course/practice.js
@@ -0,0 +1,7 @@
+function stringsToNumbers(arr) {
+    if (arr == null || arr == undefined) return []
+    return arr.map(x => parseInt(x))
+}
+
+console.log(stringsToNumbers(["1","5"]))
+console.log(stringsToNumbers())
